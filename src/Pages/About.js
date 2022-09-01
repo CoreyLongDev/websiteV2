8f369218892b8fd710a3e9b95302bd4afeda51ad
CoreyLongDev <CoreyLong90@icloud.com>
2022-09-01 09:40:14 -0400

    looking good on about page. still holding filler text for now

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,18 +1,23 @@
-// Complete rework
+import Selfie from "../Images/ProfileOne.jpeg"
 
 const About = () => {
 
     return (
-        <div className="content">
-            <h1 className="cooltext">That's Me, right?...wait</h1>
+        <>
+        <img id="selfie" src={Selfie} alt="closed and locked door detailing that this page does not exist" />
+        <div className="aboutcontent">
+            <h3 className="cooltext" id="aboutheading">That's Me!</h3>
             <br />
-            <p>This is a paragraph or so about me and the things i like and can do at the moment and that i am interested in.
+            <p id="aboutp">This is a paragraph or so about me and the things i like and can do at the moment 
+                and that i am interested in.
                 <br />
                 also this is more about me in a sexcond paragraph
                 <br />
-                i wanted this to include at least three paragraphs so lets just see how this looks without using lame ipsom whatever it is.
+                i wanted this to include at least three paragraphs 
+                so lets just see how this looks without using lame ipsom whatever it is.
             </p>
         </div>
+        </>
     )
 }
 
